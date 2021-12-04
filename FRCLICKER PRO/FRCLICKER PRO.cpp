84fb98ba16cd8f6de7b89fed08090b229633e447
Filip Rokita <filiprokita4@gmail.com>
2021-12-05 00:06:26 +0100

    push

---
+++ FRCLICKER PRO/FRCLICKER PRO.cpp
@@ -0,0 +1,63 @@
+﻿//FRCLICKER PRO
+//Release 1.0
+//Filip Rokita
+//filiprokita.com
+
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+char enable, disable;
+int cps;
+string status = "DISABLED";
+
+void menu()
+{
+	cout << "ENABLE: "; cin >> enable;
+	cout << "DISABLE: "; cin >> disable;
+	cout << "CPS: "; cin >> cps;
+}
+
+void statusindicator()
+{
+	system("cls");
+	cout << "ENABLE: " << enable << endl;
+	cout << "DISABLE: " << disable << endl;
+	cout << "CPS: " << cps << endl;
+	cout << "STATUS: " << status << endl;
+}
+
+void autoclicker()
+{
+	bool autoclicker = false;
+	while (true)
+	{
+		if (GetAsyncKeyState(enable))
+		{
+			autoclicker = true;
+			status = "ENABLED";
+			statusindicator();
+		}
+		if (GetAsyncKeyState(disable))
+		{
+			autoclicker = false;
+			status = "DISABLED";
+			statusindicator();
+		}
+		if (autoclicker == true)
+		{
+			mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+			mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+			Sleep(1000 / cps);
+		}
+	}
+}
+
+int main()
+{
+	system("title FRCLICKER PRO - Release 1.0 - filiprokita.com");
+	menu();
+	autoclicker();
+	return 0;
+}
